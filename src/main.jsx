8403e46be000bd1cf09f2160d447ddb7cafeaa4a
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2024-07-10 00:38:03 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
-
 import AddPost from "./pages/AddPost";
 import Signup from "./pages/Signup";
 import EditPost from "./pages/EditPost";
-
 import Post from "./pages/Post";
-
 import AllPosts from "./pages/AllPosts";
+import store from "./store/store.js";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
+      { path: "/", element: <Home /> },
       {
         path: "/login",
         element: (
@@ -45,7 +39,6 @@ const router = createBrowserRouter([
         path: "/all-posts",
         element: (
           <AuthLayout authentication>
-            {" "}
             <AllPosts />
           </AuthLayout>
         ),
@@ -54,7 +47,6 @@ const router = createBrowserRouter([
         path: "/add-post",
         element: (
           <AuthLayout authentication>
-            {" "}
             <AddPost />
           </AuthLayout>
         ),
@@ -63,15 +55,11 @@ const router = createBrowserRouter([
         path: "/edit-post/:slug",
         element: (
           <AuthLayout authentication>
-            {" "}
             <EditPost />
           </AuthLayout>
         ),
       },
-      {
-        path: "/post/:slug",
-        element: <Post />,
-      },
+      { path: "/post/:slug", element: <Post /> },
     ],
   },
 ]);
